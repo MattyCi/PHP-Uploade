6a9754e5b8059fe1789e64e017aa81ed03ce2c34
MattyCi <mjciaravino@gmail.com>
2017-10-06 16:31:11 -0400

    added fopen, fseek, and fwrite functionality prototype.

--- frame.php
+++ frame.php
@@ -9,6 +9,12 @@
     <input type="submit" value="Upload Image" name="submit">
 </form>
 <?php
+$myfile = fopen("./news.php", "r+") or die("Unable to open file!");
+$text = "hello there";
+// $text = "<tr><td valign="top">08/04/17</td><td><a href="/phpsite/newsroom_repository/Genetic_Testing_Requirement_Reminder.pdf" target="_blank">TESTESTES</a><img src="/phpsite/newsroom_repository/pdf.gif" alt="PDF" border="0" align="absmiddle" /></td></tr>";
+// fseek($myfile, 618); THIS IS WRITING TO THE WRONG SPOT
+fwrite($myfile, $text);
+fclose($myfile);
 ?>
 
 </body>
