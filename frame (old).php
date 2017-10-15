168f71a5d49b72116046ed4a971f3c3b12e6ca76
Matthew Ciaravino <mjciaravino@gmail.com>
2017-10-15 13:04:02 -0400

    password functionality added for frame.php

---
+++ frame (old).php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit"><br /><br />
+    Enter date (dd/mm/yyy) to be displayed along with newsroom update.
+    <input type="text" name="date" value="<?php echo date('m/d/y');?>"/><br /><br />
+    Enter the text that will display as a link to the file.
+    <input type="text" name="linktext"/>
+    <input type="hidden" name="monthyear" value="<?php echo date("F Y");?>"/>
+</form>
+November 2017
+September 2017
+December 2018
+</body>
+</html>
--- frame.php
+++ frame.php
@@ -1,17 +1,52 @@
+<?php
+
+// Define your username and password
+$username = "admin";
+$password = "password";
+
+if ($_POST['txtUsername'] != $username || $_POST['txtPassword'] != $password) {
+
+?>
+
+<h1>Login</h1>
+
+<form name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <p><label for="txtUsername">Username:</label>
+    <br /><input type="text" title="Enter your Username" name="txtUsername" /></p>
+
+    <p><label for="txtpassword">Password:</label>
+    <br /><input type="password" title="Enter your password" name="txtPassword" /></p>
+
+    <p><input type="submit" name="Submit" value="Login" /></p>
+
+</form>
+
+<?php
+
+}
+else {
+
+?>
+
 <html>
 <body>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit"><br /><br />
-    Enter date (dd/mm/yyy) to be displayed along with newsroom update.
+  <div align="left">
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select PDF to upload:
+    <input  type="file" name="fileToUpload" id="fileToUpload"><br /><br />
+    Enter date (mm/dd/yyy): <img src="./images/newsroom_date.png" alt="date" style ="vertical-align: text-bottom;">
     <input type="text" name="date" value="<?php echo date('m/d/y');?>"/><br /><br />
-    Enter the text that will display as a link to the file.
+    Enter text: <img src="./images/newsroom_text.png" alt="text" style ="vertical-align: text-bottom;">
     <input type="text" name="linktext"/>
-    <input type="hidden" name="monthyear" value="<?php echo date("F Y");?>"/>
-</form>
-November 2017
-September 2017
-December 2018
+    <input type="hidden" name="monthyear" value="<?php echo date("F Y");?>"/><br /><br />
+    <input type="submit" value="Upload PDF" name="submit">
+    </form>
+  </div>
 </body>
 </html>
+
+<?php
+
+}
+
+?>
