e8ee137781cf88490fa889551b86d0ac97937621
Matthew Ciaravino <mjciaravino@gmail.com>
2018-03-24 11:23:25 -0400

    refresh

--- test2.php
+++
@@ -1,6 +0,0 @@
-<p><strong>October 2017</strong></p><table cellpadding="5">
-    <tr>
-  <td valign="top">10/13/17</td>
-      <td><a href="/phpsite/PHP-Uploade/uploads/Provider Services Assignments by Provider Group External 082217 FINAL v2 (3).pdf" target="_blank">ForeSee Trigger Implementation Guide	</a><img src="/phpsite/newsroom_repository/pdf.gif" alt="PDF" border="0" align="absmiddle" /></td>
-    </tr>
-    </table>
